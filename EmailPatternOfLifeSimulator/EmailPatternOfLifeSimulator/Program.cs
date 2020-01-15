07ba1289223f52ff7ce1f9f56d6085aa9a5aa1a3
Joe <joseph.robson@hotmail.co.uk>
2020-01-15 22:48:33 +0000

    Created project & test project files

---
+++ EmailPatternOfLifeSimulator/EmailPatternOfLifeSimulator/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EmailPatternOfLifeSimulator {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
